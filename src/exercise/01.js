9a7f283d35755fab64083508ef02674c042c4aaa
Prashanth R <prashanthrdev@gmail.com>
2020-03-30 10:37:04 -0700

    feat(ex-01): Finish ex 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,16 +2,9 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import React from 'react'
-import {PokemonDataView} from '../utils'
-// 🐨 you'll need to import the fetchPokemon function
-// 💰 here you go:
-// import fetchPokemon from '../fetch-pokemon'
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+import { PokemonDataView, ErrorBoundary } from '../utils'
+import fetchPokemon from '../fetch-pokemon'
 
-// you'll also need the ErrorBoundary component from utils
-// 💰 here you go:
-// import {ErrorBoundary} from '../utils'
-// 💰 use it like this: <ErrorBoundary><SomeOtherComponents /></ErrorBoundary>
 
 // By default, all fetches are mocked so we can control the time easily.
 // You can adjust the fetch time with this:
@@ -21,35 +14,35 @@ import {PokemonDataView} from '../utils'
 // window.fetch.restoreOriginalFetch()
 // Note that by doing this, the FETCH_TIME will no longer be considered
 // and if you want to slow things down you should use the Network tab
-// in your developer tools to throttle your network to something like "Slow 3G"
 
-// 🐨 create the following mutable variable references (using let):
-// pokemon, pokemonError, pokemonPromise
 
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+const pokemonName = 'pikachu'
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
+let pokemonPromise, pokemonError, pokemon
 
-// 🐨 when the promise resolves, set the pokemon variable to the resolved value
-// 🐨 if the promise fails, set the pokemonError variable to the error
+pokemonPromise = fetchPokemon(pokemonName).then((
+  pokemonData => {
+    pokemon = pokemonData
+  }), 
+  errorData => {
+    pokemonError = errorData
+  })
 
-function PokemonInfo() {
-  // 🐨 if pokemonError is defined, then throw it here
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+const Fallback = () => (
+  <div>
+    Looking for Pokemon...
+  </div>
+)
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
+function PokemonInfo(props) {
+  if (pokemonError)  {
+    throw pokemonError
+  }
+
+  if (!pokemon) {
+    throw pokemonPromise
+  }
+  
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -64,12 +57,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/*
-        🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback
-        🐨 Then wrap all that with an <ErrorBoundary /> to catch errors
-        💰 I wrote the ErrorBoundary for you. You can take a look at it in the utils file if you want
-      */}
-        <PokemonInfo />
+        <ErrorBoundary>
+          <React.Suspense fallback={<Fallback />}>
+            <PokemonInfo />
+          </React.Suspense>
+        </ErrorBoundary>
       </div>
     </div>
   )
